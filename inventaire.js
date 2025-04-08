0bf0284dae936c8589ef4502c0d2d40ceb3c3152
FluxOmbre2 <142098510+FluxOmbre2@users.noreply.github.com>
2025-04-08 20:03:38 +0200

    Delete inventaire.js

--- inventaire.js
+++
@@ -1,6 +0,0 @@
-async function loadCards() {
-    // Si tu as déjà un fichier existant ailleurs
-    const response = await fetch("data/inventaires.json");  // Remplace ce chemin par celui de tes fichiers JSON existants
-    cards = await response.json();
-    displayCards(currentIndex, cardsPerLoad);
-}
